691b5c7d3f75143d95f56acf812ee0474953792a
yth01 <yth011026@gmail.com>
2020-04-24 22:39:51 +0900

    level2

---
+++ homework1/level.js
@@ -0,0 +1,9 @@
+// LEVEL 2. 자신의 조원들을 정보로 하는 JSON 배열을 만들어 출력하기
+
+var group = [
+    {name: "유태혁", age: 20},
+    {name: "조충범", age: 25},
+    {name: "심다은", age: 23},
+    {name: "최해랑", age: 27},
+    {name: "김동관", age: 26}
+]
